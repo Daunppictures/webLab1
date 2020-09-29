62a99bba686993984a99344c6b54ce48af004e2c
Daunpictures <solodovnikov.toni@gmail.com>
2020-09-29 19:05:35 +0300

    Added full responsive and animations

--- js/main.js
+++ js/main.js
@@ -2,3 +2,7 @@ function burgerActivate() {
 	let menu = document.querySelector(".menu-wrapper");
 	menu.classList.toggle("active");
 }
+function burgerAnimation() {
+	let menu = document.querySelector(".header-burger");
+	menu.classList.toggle("burger-active");
+}
